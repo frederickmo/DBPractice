81bc29b47538df4f9a7559adff8c8201baddbbda
TJ_Lty <1950698@tongji.edu.cn>
2021-07-02 21:50:04 +0800

    2021年7月2日21点49

--- Models/TrashModel.cs
+++ Models/TrashModel.cs
@@ -1,5 +1,6 @@
+using System;
 using System.Runtime.InteropServices;
-
+using DBPractice.Models;
 namespace DBPractice.Models
 {
     public class Garbage
@@ -7,6 +8,29 @@ namespace DBPractice.Models
         public string id { get; set; }
         public string type { get; set; }
         public char result { get; set; }
-        public DataTime   
+        public DateTime finishtime { get; set; }
     }
+
+    public class ThrowGarbage
+    {
+        public string username { get; set; }
+        public string garbageid { get; set; }
+        public char status { get; set; }
+        public DateTime throwtime { get; set; }
+    }
+
+    public class TransportStart
+    {
+        public string garbageid { get; set; }
+        public string truckid { get; set; }
+        public DateTime starttime { get; set; }
+    }
+
+    public class TransportEnd
+    {
+        public string garbageid { get; set; }
+        public string truckid { get; set; }
+        public DateTime endtime { get; set; }
+    }
+    
 }
